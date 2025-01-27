302cc9411c8da7d0024769899559a7b5a397f824
Yussuf <you@example.com>
2025-01-27 15:44:15 +0100

    moved all the routes into routes files

--- index.js
+++ index.js
@@ -5,48 +5,10 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 // routes
-import {
-  createUser,
-  deleteUser,
-  getSingleUser,
-  getAllUsers,
-  loginUser,
-  logoutUser,
-  updateUserProfile,
-  makeUserAdmin,
-  verifyUser,
-  assignRole,
-} from "./controllers/users.js";
-import {
-  approveCourseByAdmin,
-  createCourse,
-  deleteCourse,
-  deleteCoursesByAnInstructor,
-  filterCourses,
-  getAllCoursesAvailable,
-  getAllCoursesByAnInstructor,
-  getSingleCourse,
-  publishCourseByInstructor,
-  updateCourse,
-} from "./controllers/course.js";
-import {
-  createLecture,
-  deleteAllLectureInACourse,
-  deleteSingleLecture,
-  getAllLecturesInACourse,
-  getSingleLecture,
-  updateLecture,
-} from "./controllers/lectures.js";
-
-// middlewares
-import { verifyToken } from "./middlewares/verifyToken.js";
-import upload from "./middlewares/fileUpload.js";
-import {
-  changePassword,
-  createNewPassword,
-  requestResetPasswordLink,
-  resetPassword,
-} from "./controllers/password.js";
+import authRouter from "./routes/auth.js";
+import userRouter from "./routes/users.js";
+import courseRouter from "./routes/course.js";
+import lectureRouter from "./routes/lecture.js";
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -54,63 +16,18 @@ const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(`Error Connecting to MongoDb ${err}`));
 
-// User Routes
-app.post("/register", createUser);
-app.post("/login", loginUser);
-app.get("/verify-email", verifyUser);
-app.post("/logout", logoutUser);
-app.get("/user/:id", getSingleUser);
+app.use("/api/users", userRouter);
+app.use("/api", authRouter);
+app.use("/api/courses", courseRouter);
+app.use("/api/lectures", lectureRouter);
 
-// Protected User Routes
-app.put("/user/:id/make-admin", verifyToken, makeUserAdmin);
-app.put("/user/:id/role", verifyToken, assignRole);
-
-app.get("/users", verifyToken, getAllUsers);
-app.put("/users/:id", verifyToken, updateUserProfile);
-app.put("/user/:id", verifyToken, deleteUser);
-app.put("/change-password", verifyToken, upload.none(), changePassword);
-app.put("/request-reset", verifyToken, upload.none(), requestResetPasswordLink);
-app.get("/reset-password", upload.none(), createNewPassword);
-app.post("/reset-password", upload.none(), resetPassword);
-
-// Course Routes
-app.get("/courses/:id", getSingleCourse);
-app.get("/courses/instructor/:instructor", getAllCoursesByAnInstructor);
-app.get("/courses", filterCourses);
-
-// Protected Course Routes
-app.get("/get-courses-by-admin", verifyToken, getAllCoursesAvailable);
-app.post("/course", verifyToken, upload.single("image"), createCourse);
-app.put("/course/:id", verifyToken, updateCourse);
-app.put("/course/:id", verifyToken, deleteCourse);
-app.put(
-  "/courses/instructor/:instructor",
-  verifyToken,
-  deleteCoursesByAnInstructor
-);
-app.put("/approve-course/:id", verifyToken, approveCourseByAdmin);
-app.put("/publish-course/:id", verifyToken, publishCourseByInstructor);
-
-// Protected Lectures Routes
-app.post(
-  "/:courseId/lecture",
-  verifyToken,
-  upload.single("video"),
-  createLecture
-);
-app.put("/course/lectures/:id", verifyToken, deleteSingleLecture);
-app.put("/:id/lectures", verifyToken, deleteAllLectureInACourse);
-app.put("/lectures/:id", verifyToken, upload.single("video"), updateLecture);
-
-// Lecure Routes
-app.get("/course/lectures/:id", getSingleLecture);
-app.get("/course/:id", getAllLecturesInACourse);
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,28 @@
+import express from "express";
+
+import { verifyToken } from "../middlewares/verifyToken.js";
 import {
   createUser,
   deleteUser,
   getSingleUser,
   getAllUsers,
   loginUser,
-  logoutUser,
   updateUserProfile,
   makeUserAdmin,
-  verifyUser,
   assignRole,
-} from "./controllers/users.js";
+} from "../controllers/users.js";
+
+const router = express.Router();
+
+// User Routes
+router.post("/register", createUser);
+router.post("/login", loginUser);
+router.get("/user/:id", getSingleUser);
+router.put("/user/:id/make-admin", verifyToken, makeUserAdmin);
+
+router.put("/:id/role", verifyToken, assignRole);
+router.get("/", verifyToken, getAllUsers);
+router.put("/:id", verifyToken, updateUserProfile);
+router.put("/:id", verifyToken, deleteUser);
+
+export default router;
