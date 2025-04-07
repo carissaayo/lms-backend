9dc5ba7d296a7b5b2206dfe8e0187e26aa288b3d
Yussuf <you@example.com>
2025-04-07 10:34:33 +0100

    still on the render issue

--- routes/student.js
+++ routes/student.js
@@ -4,6 +4,7 @@ import { verifyToken } from "../middlewares/verifyToken.js";
 import {
   createUser,
   deleteUser,
+  getSingleUser,
   getAllUsers,
   loginUser,
   updateUserProfile,
@@ -17,8 +18,8 @@ const router = express.Router();
 // User Routes
 router.post("/register", createUser);
 router.post("/login", loginUser);
-
-router.get("/user/:id", verifyToken, getStudentDetails);
+router.get("/user/:id", verifyToken, getSingleUser);
+router.get("/user-details/:id", verifyToken, getStudentDetails);
 router.get("/user-by-admin/:id", verifyToken, getSingleUserByAdmin);
 router.put("/user/:id/make-admin", verifyToken, makeUserAdmin);
 
